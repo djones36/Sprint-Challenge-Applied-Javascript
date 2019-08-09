ae737cfe3cb4895726df583502a9928156a4f302
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-09 08:53:41 -0700

    finished header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,30 +9,32 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header(){
+
     //query select element
-    const headerParent = document.querySelector('.header-container');
-    headerParent.appendChild(headerCreator());
-    function headerCreator(){
-    //Define new elements
-    const header = document.createElement('div');
-    const spanOne = document.createElement('span');
-    const h1 = document.createElement('h1');
-    const spanTwo = document.createElement('span');
-    //set class names
-    header.classList.add('header');
-    spanOne.classList.add('date');
-    spanTwo.classList.add('temp');
-    //set textcontnent
-    spanTwo.textContent = "MARCH 28, 2019";
-    h1.textContent = "Lambda Time";
-    spanTwo.textContent = "98°";
-    //setup structure of elements
-    headerParent.appendChild(header);
-    header.appendChild(spanOne);
-    header.appendChild(h1);
-    header.appendChild(spanTwo);
+const headerParent = document.querySelector(".header-container");
+headerParent.appendChild(headerCreator());
     
-    return header;
+    function headerCreator(){
+        //Define new elements
+        const header = document.createElement('div');
+        const spanOne = document.createElement('span');
+        const h1 = document.createElement('h1');
+        const spanTwo = document.createElement('span');
+        //set class names
+        header.classList.add('header');
+        spanOne.classList.add('date');
+        spanTwo.classList.add('temp');
+        //set textcontnent
+        spanOne.textContent = "SMARCH 28, 2019";
+        h1.textContent = "Lambda Time";
+        spanTwo.textContent = "98°";
+        //setup structure of elements
+        // headerParent.appendChild(header);
+        header.appendChild(spanOne);
+        header.appendChild(h1);
+        header.appendChild(spanTwo);
+        
+        return header;
     };
-}
+
+
